38ebba2c245607014b6530e031c8616bb40051ff
Forrestbob <forrestbob2000@yahoo.com>
2017-02-27 17:00:02 -0500

    connect db update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,9 +1,9 @@
 <?php
   // 1. Create a database connection
   $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpass = "";
-  $dbname = "test_db";
+  $dbuser = "DataBAEs";
+  $dbpass = "DataBAEs";
+  $dbname = "smart_fridge";
   $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
   // Test if connection succeeded
   if(mysqli_connect_errno()) {
