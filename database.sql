dd21046d5e56f039d00e753f9fc8a0a2aeb398a9
eelizan1 <eelizan1@student.gsu.edu>
2017-02-27 16:34:37 -0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test_db
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `customerID` int(11) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`customerID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1001,'Enrico','Elizan','eelizan1@student.gsu.edu','5315 Derby Chase Court');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `ordersID` int(11) NOT NULL AUTO_INCREMENT,
+  `productID` int(11) DEFAULT NULL,
+  `customerID` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `dateordered` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ordersID`),
+  KEY `productID_fk_idx` (`productID`),
+  CONSTRAINT `productID_fk` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1009 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1007,1001,1001,NULL,'2017-02-27 21:23:39',1),(1008,1001,1001,NULL,'2017-02-27 21:24:39',1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `productID` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`productID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1001,'product1',7,5.00),(1002,'product2',15,7.00),(1003,'product3',7,2.00);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-27 16:31:47
---
+++ order_process.php
@@ -0,0 +1,53 @@
+<?php
+  // 1. Create a database connection
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "";
+  $dbname = "test_db";
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  // Test if connection succeeded
+  if(mysqli_connect_errno()) {
+    die("Database connection failed: " . 
+         mysqli_connect_error() . 
+         " (" . mysqli_connect_errno() . ")"
+    );
+  }
+?>
+    
+    <?php
+    if(isset($_POST["submit"])){   
+    
+
+    
+    
+    $query2 = "INSERT INTO orders (ordersID, productsID, customerID, price, dateordered, quantity)
+                VALUES(ordersID = ordersID + 1, productsID, customerID, price = price + 1, quantity = quantity + 1)"; 
+    
+       
+ $updateResult = mysqli_query($connection, $query2);
+        
+         if ($updateResult) {
+		echo "Success!"; 
+	} else {
+        
+        die("Database query failed. " . mysqli_error($connection));
+    }        
+        
+    }
+
+	// Test if there was a query error
+
+
+	?>
+
+<html>
+
+<?php
+		
+		// you don't use mysqli_free_result because it returns a boolran 
+		
+	?>
+	
+</html>
+
+<?php  mysqli_close($connection) ?> 
